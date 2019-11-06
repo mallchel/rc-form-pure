706f5e85efbc23c5f9f4c5d6cfef475e85d7a3c5
mallchel <sebastianalfabc@gmail.com>
2019-11-06 14:48:07 +0300

    fix - transpile

--- src/modules/FormBuilder/_tests/UI/fieldsConfig.js
+++ src/modules/FormBuilder/_tests/UI/fieldsConfig.js
@@ -1,45 +1,45 @@
 import React from 'react';
 
-class TextField extends React.PureComponent {
-  render() {
-    const { value, type, error, onChange, required, validator } = this.props;
+// class TextField extends React.PureComponent {
+//   render() {
+//     const { value, type, error, onChange, required, validator } = this.props;
 
-    return (
-      <React.Fragment>
-        <input key={type} value={value} onChange={e => onChange(e.target.value)} />
-        {error}
-      </React.Fragment>
-    );
-  }
-}
+//     return (
+//       <React.Fragment>
+//         <input key={type} value={value} onChange={e => onChange(e.target.value)} />
+//         {error}
+//       </React.Fragment>
+//     );
+//   }
+// }
 
-export default {
-  // firstName: {
-  //   rules: [
-  //     { required: true, message: 'Please fill firstName' },
-  //     {
-  //       validator: (rules, value, callback) => {
-  //         callback('Error validator!!');
-  //       },
-  //     },
-  //   ],
-  //   children: props => <TextField {...props} />,
-  // },
-  // lastName: {
-  //   children: props => <TextField {...props} />,
-  // },
-  // email: {
-  //   rules: [
-  //     { required: true, message: 'Please fill email' },
-  //     { type: 'email', message: 'incorrect email' },
-  //   ],
-  //   children: props => <TextField {...props} />,
-  // },
-  // password: {
-  //   rules: [
-  //     { required: true, message: 'Please fill password' },
-  //     { len: 10, message: 'string length must be equal 10' },
-  //   ],
-  //   children: props => <TextField {...props} />,
-  // },
-};
+// export default {
+// firstName: {
+//   rules: [
+//     { required: true, message: 'Please fill firstName' },
+//     {
+//       validator: (rules, value, callback) => {
+//         callback('Error validator!!');
+//       },
+//     },
+//   ],
+//   children: props => <TextField {...props} />,
+// },
+// lastName: {
+//   children: props => <TextField {...props} />,
+// },
+// email: {
+//   rules: [
+//     { required: true, message: 'Please fill email' },
+//     { type: 'email', message: 'incorrect email' },
+//   ],
+//   children: props => <TextField {...props} />,
+// },
+// password: {
+//   rules: [
+//     { required: true, message: 'Please fill password' },
+//     { len: 10, message: 'string length must be equal 10' },
+//   ],
+//   children: props => <TextField {...props} />,
+// },
+// };
