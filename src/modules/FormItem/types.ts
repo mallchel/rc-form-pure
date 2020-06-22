64ea0644ede0b2517f3d14c4c1428af4eac9672d
qramilq <qramilq@mail.ru>
2020-06-22 17:02:38 +0300

    fix: WrapperPropTypes expanded  and OnChangeFieldsType exported

--- src/modules/FormItem/types.ts
+++ src/modules/FormItem/types.ts
@@ -2,15 +2,16 @@ import { AllHTMLAttributes } from 'react';
 
 import { OnChangeType, RegisterFieldType, ValidateType, ErrorMessageType, ComponentPropTypes, IField } from '../types';
 
-export type WrapperPropTypes = AllHTMLAttributes<any> & {
-  name: string;
-  component?: ComponentPropTypes<any>;
-  formatter?: (value: any) => any;
-  validateOnBlur?: boolean;
-  validate?: ValidateType;
-  errorMessage?: ErrorMessageType;
-  initialValue?: any;
-};
+export type WrapperPropTypes = AllHTMLAttributes<any> &
+  any & {
+    name: string;
+    component?: ComponentPropTypes<any>;
+    formatter?: (value: any) => any;
+    validateOnBlur?: boolean;
+    validate?: ValidateType;
+    errorMessage?: ErrorMessageType;
+    initialValue?: any;
+  };
 
 export type PropTypesFormItem = Omit<WrapperPropTypes, 'onChange'> & {
   registerField: (field: RegisterFieldType) => void;
